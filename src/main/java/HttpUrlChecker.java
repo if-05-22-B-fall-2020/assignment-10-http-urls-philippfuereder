607f1e9128994b50a08f857f4bde52502b92a626
philippfuereder <43365816+philippfuereder@users.noreply.github.com>
2021-06-16 16:18:02 +0200

    message

--- src/main/java/HttpUrlChecker.java
+++ src/main/java/HttpUrlChecker.java
@@ -27,8 +27,7 @@ import java.util.regex.Pattern;
 
 public class HttpUrlChecker {
     public static Matcher getMatcher(String httpString) {
-        //Pattern pattern = Pattern.compile("^((http)|(https))://(www.?)[a-zA-z0-9]*.([a-z][a-z]*)$");
-        Pattern pattern = Pattern.compile("^(http)(://)[.]*(www.?)[a-zA-z0-9]*(.)(com)");
+        Pattern pattern = Pattern.compile("^(http)(s?)://[.]*(www.?)[a-zA-z0-9]*(.)(com)");
         return pattern.matcher(httpString);
     }
 }
